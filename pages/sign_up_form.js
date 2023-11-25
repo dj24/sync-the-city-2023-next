821f637175572555f023c8d1250857c1660f3c3a
Daniel Jackson <danieljackson97123@gmail.com>
2023-11-25 12:24:42 +0000

    update form styles

--- pages/sign_up_form.js
+++ pages/sign_up_form.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Sign_up_form() {
   return (
@@ -11,26 +12,28 @@ export default function Sign_up_form() {
         height: "100vh",
       }}
     >
-      <div className="container">
+      <div className="container flex flex-col items-center gap-4">
+        <Image
+          width={400}
+          height={400}
+          src="/buddylogo.png"
+          alt={"buddy"}
+          className="-mb-16 -mt-16"
+        />
         <h1>Sign up to register your interest!</h1>
         <form
           target="_blank"
           action="https://formsubmit.co/syncthecitysignup@gmail.com"
           method="POST"
+          className="flex flex-col items-center gap-4"
         >
-          <div className="form-group">
-            <div className="form-row">
-              <div className="col">
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control"
-                  placeholder="Email Address"
-                  required
-                />
-              </div>
-            </div>
-          </div>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            placeholder="Email Address"
+            required
+          />
           <div>
             <button className="flex-1 py-4">Submit Form</button>
             {/*<button type="Continue" className="btn btn-lg btn-dark btn-block bg-red-300">*/}
