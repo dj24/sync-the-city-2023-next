b7f3ecfad5f88e321eea4ca80e1e7cb8c9c830f5
Archie Smith <archie.smith.as@icloud.com>
2023-11-25 11:22:42 +0000

    functionality?

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,8 @@
-export default function Test() {
+import Link from "next/link";
+
+export default function Login() {
   return (
-    <main className="flex flex-col items-center justify-start h-screen bg-[#F1E5FF]">
+    <div>
       <div className="bg-yellow-400 dark:bg-gray-800 h-screen overflow-hidden flex items-center justify-center">
         <div className="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl rounded-xl">
           <div className="bg-gray-800 shadow shadow-gray-200 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
@@ -31,15 +33,21 @@ export default function Test() {
                 placeholder="Password"
               />
             </div>
-            <a
-              href="bank.html"
+            <Link
+              href={"/bank.html"}
               className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded"
             >
               Login
-            </a>
+            </Link>
+            <Link
+              href={"/"}
+              className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded"
+            >
+              Sign Up
+            </Link>
           </form>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
